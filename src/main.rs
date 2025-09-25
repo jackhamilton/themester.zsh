4f884175e2cff7374eb7a059cd0326b77e146dd9
Jack Hamilton <jackham800@gmail.com>
2025-09-25 15:27:59 -0500

    Publish updates to freezable and add randomization to config

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use freezable_trait::Freezable;
 use std::fs;
 use std::fs::OpenOptions;
 use std::path::Path;
 use rand::SeedableRng;
 use std::env;
+use chrono::{DateTime, TimeDelta, Utc};
 use rand::seq::IndexedRandom;
 use rand::rngs::StdRng;
 use freezable_trait::freezable;
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 #[freezable]
 struct Config {
     cache_file_location: String,
@@ -16,18 +18,22 @@ struct Config {
     nvim_plugin_env_var_name: String,
     write_term_lua: bool,
     term_lua_path: String,
+    // Zero means it will never randomize
+    hours_per_randomization: i32,
     theme: Vec<Theme>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Self {
+            _unknown_fields: [].into(),
             cache_file_location: "~/.config/themester/.themecache".into(),
             term_env_var_name: "TERM_THEME".into(),
             nvim_theme_env_var_name: "NVIM_THEME".into(),
             nvim_plugin_env_var_name: "NVIM_THEME_PLUGIN".into(),
             write_term_lua: true,
             term_lua_path: "~/.config/wezterm/dynamic_theme.lua".into(),
+            hours_per_randomization: 0,
             theme: vec![
                 Theme::default()
             ]
@@ -35,7 +41,7 @@ impl Default for Config {
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Debug)]
 #[freezable]
 struct Theme {
     term: String,
@@ -46,9 +52,25 @@ struct Theme {
 impl Default for Theme {
     fn default() -> Self {
         Self {
+            _unknown_fields: [].into(),
             term: "plugin-name".into(),
             nvim_plugin: "nvim-plugin-name".into(),
             nvim_themename: "nvim-plugin-colorscheme".into()
+        }
+    }
+}
+
+#[freezable]
+struct LocalData {
+    last_randomized: DateTime<Utc>,
+}
+
+impl Default for LocalData {
+    fn default() -> Self {
+        Self {
+            _unknown_fields: [].into(),
+            // last_randomized: chrono::offset::Utc::now()
+            last_randomized: DateTime::from_timestamp_secs(0).expect("Could not resolve datetime")
         }
     }
 }
@@ -61,8 +83,9 @@ fn main() {
     let arg = args[1].clone();
     match arg.as_str() {
         "-h" => help(),
-        "-r" => randomize_theme(),
+        "-r" => randomize_theme(None),
         "-l" => load_cache(),
+        "-v" => version(),
         _ => help(),
     }
 }
@@ -73,8 +96,23 @@ fn help() {
     std::process::exit(0);
 }
 
+fn version() {
+    println!("themester.zsh version {} by Jack Hamilton", env!("CARGO_PKG_VERSION"));
+    std::process::exit(0);
+}
+
 fn load_cache() {
     let config: Config = toml_configurator::get_config("themester".into());
+
+    let local_data = LocalData::from_file_str("~/.config/themester/.localdata");
+    if config.hours_per_randomization > 0
+       && let Some(delta) = TimeDelta::new((config.hours_per_randomization * 3600).into(), 0)
+       && let Some(cutoff) = local_data.last_randomized.checked_add_signed(delta)
+       && cutoff < chrono::offset::Utc::now() {
+        randomize_theme(Some(config.clone()));
+        return
+    }
+
     let expanded = shellexpand::tilde(&config.cache_file_location).into_owned().to_string();
     let file_path = Path::new(&expanded);
     let cache_contents = fs::read_to_string(file_path).expect("echo \"Could not read cache file!\"");
@@ -82,8 +120,10 @@ fn load_cache() {
     println!("{cache_contents}");
 }
 
-fn randomize_theme() {
-    let config: Config = toml_configurator::get_config("themester".into());
+fn randomize_theme(config: Option<Config>) {
+    let config: Config = config.or_else(|| {
+        toml_configurator::get_config("themester".into())
+    }).expect("Could not resolve config");
 
     let export_str = randomize(&config);
     let expanded = shellexpand::tilde(&config.cache_file_location).into_owned().to_string();
@@ -92,9 +132,15 @@ fn randomize_theme() {
         Ok(_) => Ok(()),
         Err(e) => Err(e),
     }.unwrap_or_else(|why| {
-        println!("! {:?}", why.kind());
-    });
+            println!("! {:?}", why.kind());
+        });
     fs::write(file_path, export_str.clone()).expect("echo \"Unable to write theme lua file.\"");
+
+    let data = LocalData {
+        last_randomized: chrono::offset::Utc::now(),
+        _unknown_fields: [].into(),
+    };
+    data.write_to_file_str("~/.config/themester/.localdata");
 
     println!("{export_str}");
 }
@@ -109,16 +155,15 @@ fn randomize(config: &Config) -> String {
     let mut rng = StdRng::from_os_rng();
     if let Some(existing) = env::var_os(config.term_env_var_name.clone()) {
         for _ in 0..retries {
-            if let Some(chosen) = config.theme.iter().map(|item| item.term.clone()).collect::<Vec<String>>().choose(&mut rng) {
-                if **chosen != *existing {
-                    export_string = format!("{}export {}=\"{}\"", export_string, config.term_env_var_name, chosen);
-                    write_term_themefile_lua(config.write_term_lua, config.term_lua_path.clone(), String::from(chosen));
-                    if let Some(nvim) = config.theme.iter().find(|item| item.term == *chosen) {
-                        export_string = format!("{}\nexport {}=\"{}\"", export_string, config.nvim_theme_env_var_name, nvim.nvim_themename);
-                        export_string = format!("{}\nexport {}=\"{}\"", export_string, config.nvim_plugin_env_var_name, nvim.nvim_plugin);
-                    }
-                    return export_string;
+            if let Some(chosen) = config.theme.iter().map(|item| item.term.clone()).collect::<Vec<String>>().choose(&mut rng)
+            && **chosen != *existing {
+                export_string = format!("{}export {}=\"{}\"", export_string, config.term_env_var_name, chosen);
+                write_term_themefile_lua(config.write_term_lua, config.term_lua_path.clone(), String::from(chosen));
+                if let Some(nvim) = config.theme.iter().find(|item| item.term == *chosen) {
+                    export_string = format!("{}\nexport {}=\"{}\"", export_string, config.nvim_theme_env_var_name, nvim.nvim_themename);
+                    export_string = format!("{}\nexport {}=\"{}\"", export_string, config.nvim_plugin_env_var_name, nvim.nvim_plugin);
                 }
+                return export_string;
             }
         }
     } else if let Some(chosen) = config.theme.iter().map(|item| item.term.clone()).collect::<Vec<String>>().choose(&mut rng) {
